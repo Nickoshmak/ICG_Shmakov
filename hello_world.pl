91e72738fb089e94a22c210938358cd2bda11a9c
Nickoshmak <57125957+Nickoshmak@users.noreply.github.com>
2020-01-09 12:26:42 +0700

    Update hello_world.pl

--- hello_world.pl
+++ hello_world.pl
@@ -1,4 +1,17 @@
 use strict;
 use warnings;
+use Bio::SeqIO;
 
 print "wtf world\n";
+
+sub rand_nuc{
+  
+  }
+
+sub randseq{
+  my $seq = '';
+  for (my $i = 0; $i < $_[0]; $i++){
+    $seq = $seq . rand_nuc();
+    }
+  return $seq;
+  }
